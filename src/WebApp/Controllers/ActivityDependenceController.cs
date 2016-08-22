53a37495edf0503058d7493b3b9c396eb3d9e3e7
Bzway <zhumingwu@hotmail.com>
2016-08-23 00:05:55 +0800

    changes

---
+++ src/WebApp/Controllers/ActivityDependenceController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ActivityDependenceController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ActivityDependence
+        public ActionResult Index()
+        {
+            var activityDependences = db.ActivityDependences.Include(a => a.BackwardActivity).Include(a => a.ForwardActivity);
+            return View(activityDependences.ToList());
+        }
+
+        // GET: ActivityDependence/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityDependence activityDependence = db.ActivityDependences.Find(id);
+            if (activityDependence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityDependence);
+        }
+
+        // GET: ActivityDependence/Create
+        public ActionResult Create()
+        {
+            ViewBag.ForwardActivityId = new SelectList(db.Activities, "Id", "Name");
+            ViewBag.BackwardActivityId = new SelectList(db.Activities, "Id", "Name");
+            return View();
+        }
+
+        // POST: ActivityDependence/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ForwardActivityId,BackwardActivityId,DependenceType")] ActivityDependence activityDependence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ActivityDependences.Add(activityDependence);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ForwardActivityId = new SelectList(db.Activities, "Id", "Name", activityDependence.ForwardActivityId);
+            ViewBag.BackwardActivityId = new SelectList(db.Activities, "Id", "Name", activityDependence.BackwardActivityId);
+            return View(activityDependence);
+        }
+
+        // GET: ActivityDependence/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityDependence activityDependence = db.ActivityDependences.Find(id);
+            if (activityDependence == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ForwardActivityId = new SelectList(db.Activities, "Id", "Name", activityDependence.ForwardActivityId);
+            ViewBag.BackwardActivityId = new SelectList(db.Activities, "Id", "Name", activityDependence.BackwardActivityId);
+            return View(activityDependence);
+        }
+
+        // POST: ActivityDependence/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ForwardActivityId,BackwardActivityId,DependenceType")] ActivityDependence activityDependence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activityDependence).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ForwardActivityId = new SelectList(db.Activities, "Id", "Name", activityDependence.ForwardActivityId);
+            ViewBag.BackwardActivityId = new SelectList(db.Activities, "Id", "Name", activityDependence.BackwardActivityId);
+            return View(activityDependence);
+        }
+
+        // GET: ActivityDependence/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityDependence activityDependence = db.ActivityDependences.Find(id);
+            if (activityDependence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityDependence);
+        }
+
+        // POST: ActivityDependence/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ActivityDependence activityDependence = db.ActivityDependences.Find(id);
+            db.ActivityDependences.Remove(activityDependence);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/WebApp/Models/IdentityModels.cs
@@ -0,0 +1,86 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WebApplication1.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+    [Table("Activity")]
+    public class Activity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? DU { get; set; }
+        public DateTime? ES { get; set; }
+        public DateTime? EF { get; set; }
+        public DateTime? LS { get; set; }
+        public DateTime? LF { get; set; }
+        public int? Slack { get; set; }
+        public int? ParentId { get; set; }
+        
+        [ForeignKey("ParentId")]
+        public virtual Activity Parent { get; set; }
+        public virtual ICollection<Activity> SubActivities { get; set; }
+        
+        [ForeignKey("ForwardActivityId")]
+        public ICollection<ActivityDependence> BackwardActivity { get; set; }
+        [ForeignKey("BackwardActivityId")]
+        public ICollection<ActivityDependence> ForwardActivity { get; set; }
+    }
+    [Table("ActivityDependence")]
+    public class ActivityDependence
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? ForwardActivityId { get; set; }
+
+        public int? BackwardActivityId { get; set; }
+        public DependenceType DependenceType { get; set; }
+        [ForeignKey("BackwardActivityId")]
+        public virtual Activity ForwardActivity { get; set; }
+        [ForeignKey("ForwardActivityId")]
+        public virtual Activity BackwardActivity { get; set; }
+    }
+    public enum DependenceType
+    {
+        Finish2Start = 0,
+        Finish2Finish,
+        Start2Start,
+        Start2Finish,
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityDependence> ActivityDependences { get; set; }
+    }
+}
